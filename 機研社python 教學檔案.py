bbd7682807a2670f295086b4878676e39c904cb5
JavaOliverLu <70323169+JavaOliverLu@users.noreply.github.com>
2021-12-08 13:54:13 +0800

    Create 機研社python 教學檔案.py

---
+++ 機研社python 教學檔案.py
@@ -0,0 +1,34 @@
+for i in range(10):
+  print(i)
+
+for i in range(2,5):
+  print(i)
+
+for i in range(1,99,3):
+  print(i)
+
+a=1
+if (a>7):
+  print("a")
+elif(a<7):
+  print("s")
+else:
+  print("hit")
+
+bc=1
+while(bc<10):
+  print(bc)
+  bc=bc+1
+ 
+#while (True):
+ # print("hi")
+
+def chemistry(x):
+  if (x>7):
+    print("a")
+  elif(x<7):
+   print("s")
+  else:
+    print("hit")
+chemistry(3)
+chemistry(7)
